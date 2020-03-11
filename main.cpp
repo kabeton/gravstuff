182895102f24355d8230e2dcfb4e402a67fb5031
Ilya <frolov.ilia@phystech.edu>
2020-03-11 18:29:05 +0300

    Multi-stage optimization
    multi-stage optimization is added

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include"renderer.h"
 #include "Parser.h"
 #include"grid.h"
+//std::vector<Point> Grid::data = std::vector<Point>();
 int sz = 420; // количество строк в файле
 deque<Object> container; //здесь храним орбиты
 Renderer renderer = Renderer(&container);
@@ -45,11 +46,46 @@ void show(std::vector<Point> dt) //костыль для вывода вектора на экран
 }
 int main(int argc, char *argv[])
 {
-    Grid grid = Grid();
-    grid.set_angle_range(0, 30);
-    grid.set_velocity_range(0, 30);
-    grid.set_grid_size(3, 3);
-    show(grid.calculate());
+    double min_angle = 0;
+    double max_angle = 360;
+    double angle_range = 360;
+    double min_velocity = 0;
+    double max_velocity = 1000;
+    double velocity_range = 1000;
+    double start_angle = angle_range / 2;
+    double start_velocity = velocity_range / 2;
+    double range[4];
+    range[0] = velocity_range / 2.0;
+    range[1] = velocity_range / 2.0;
+    range[2] = angle_range / 2.0;
+    range[3] = angle_range / 2.0;
+    double boundary[4];
+    std::vector<Point> result;
+    for (int h = 1; h < 10; ++h)
+    {
+        Grid grid = Grid();
+        grid.set_angle_range(start_angle - range[2], start_angle + range[3]);
+        grid.set_velocity_range(start_velocity - range[0], start_velocity + range[1]);
+        grid.set_grid_size(3, 3);
+        result = grid.calculate();
+        start_angle = result[0].angle;
+        start_velocity = result[0].initial_velocity;
+        range[0] = range[0] / grid.num_divisions;
+        range[1] = range[1] / grid.num_divisions;
+        range[2] = range[2] / grid.num_divisions;
+        range[3] = range[3] / grid.num_divisions;
+        boundary[0] = result[0].initial_velocity - min_velocity;
+        boundary[1] = max_velocity - result[0].initial_velocity;
+        boundary[2] = result[0].angle - min_angle;
+        boundary[3] = max_angle - result[0].angle;
+        if (range[0] > boundary[0]) range[0] = boundary[0];
+        if (range[1] > boundary[1]) range[1] = boundary[1];
+        if (range[2] > boundary[2]) range[2] = boundary[2];
+        if (range[3] > boundary[3]) range[3] = boundary[3];
+        //show(result);
+    }
+    std::cout<<"****"<<std::endl;
+    show(Grid::data);
     create_window(argc, argv, "window1", 600, 300);
     Object obj =Object(1, 1);
     obj.init_from_file("C:\\Users\\frolo\\Documents\\codeblocks\\visual_model\\data.csv", sz);
