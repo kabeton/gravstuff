07fd5f2b680075890553522eedccac77662069b9
kabeton <58085917+kabeton@users.noreply.github.com>
2020-03-18 16:45:39 +0300

    working implementation of system and get_solution

---
+++ grr.h
@@ -0,0 +1,113 @@
+#ifndef GRID_H_INCLUDED
+#define GRID_H_INCLUDED
+#include<string>
+#include<algorithm>
+#include <unistd.h>
+#include <sys/types.h>
+#include <iostream>
+#include <fstream>
+#include <sys/wait.h>
+string path = "C:\\Users\\frolo\\Documents\\codeblocks\\visual_model\\data.csv";//для отладки
+double get_solution(double angle, double velocity, string *_path)
+{
+    string ofname;
+    ofname = to_string(velocity) + "_" + to_string(angle);
+    *_path = "/home/kabe/infa/scimod/gravi/" + ofname;
+    pid_t id = fork();
+    pid_t pid;
+    int status;
+    if(id == 0) {
+      (void)execl("/home/kabe/infa/scimod/gravi/a.out", "/home/kabe/infa/scimod/gravi/a.out", to_string(velocity).c_str(), to_string(angle).c_str(), (char *)0);
+    } 
+    pid = waitpid(-1, &status, 0);
+    ifstream spd("speed");
+    double spd_fin;
+    spd >> spd_fin; 
+    spd.close();
+    return spd_fin;
+}
+class Point
+{
+public:
+    double initial_velocity;
+    double angle;
+    double final_velocity;
+    string path;
+    bool operator<(Point _point)
+    {
+        return (final_velocity < _point.final_velocity);
+    }
+};
+class Grid
+{
+public:
+    static std::vector<Point> data;
+    double min_angle;
+    double max_angle;
+    double min_velocity;
+    double max_velocity;
+    int num_divisions = 2;
+    int num_brunch = 1;
+    int depth = 4;
+    int grid_size1 = 10;
+    int grid_size2 = 10;
+    void set_velocity_range(double v1, double v2)
+    {
+        min_velocity = v1;
+        max_velocity = v2;
+    }
+    void set_angle_range(double a1, double a2)
+    {
+        min_angle = a1;
+        max_angle = a2;
+    }
+    void set_param(int divisions, int brunch, int _depth)
+    {
+        num_divisions = divisions;
+        num_brunch = brunch;
+        depth = _depth;
+    }
+    void set_grid_size(int a, int b)
+    {
+        grid_size1 = a;
+        grid_size2 = b;
+    }
+    std::vector<Point> calculate()
+    {
+        std::vector<Point> buf;
+        double angle_step = (max_angle - min_angle) / grid_size1;
+        double velocity_step = (max_velocity - min_velocity) / grid_size2;
+        for(double i = min_angle; i <= max_angle; i += angle_step)
+        {
+            for(double g = min_velocity; g <= max_velocity; g += velocity_step)
+            {
+                Point point = Point();
+                point.angle = i;
+                point.initial_velocity = g;
+                point.final_velocity = get_solution(i, g, &(point.path));
+                data.push_back(point);
+                if (buf.size() < num_brunch)
+                {
+                    buf.push_back(point);
+                    std::sort(buf.begin(), buf.end());
+                }
+                else
+                {
+                    for(auto it = buf.begin(); it != buf.end(); ++it)
+                    {
+                        if(point.final_velocity > (*it).final_velocity)
+                        {
+                            (*it) = point;
+                            std::sort(buf.begin(), buf.end());
+                        }
+                    }
+                }
+            }
+        }
+        return buf;
+    }
+
+};
+std::vector<Point> Grid::data = std::vector<Point>();
+
+#endif // GRID_H_INCLUDED
---
+++ sysfin.cpp
@@ -0,0 +1,170 @@
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <fstream>
+
+typedef struct planet
+{
+	double m;
+	double x;
+	double y;
+	double v;
+	double vx;
+	double vy;
+	double r;
+	double phi;	
+	double omega;
+	double per;
+}planet;
+
+using namespace std;
+int main(int argc, char *argv[], char *envp[])
+{
+  string ofname;
+  double v0, angle;
+  if(argc > 1) {
+    v0 = stod(argv[1]);
+    angle = stod(argv[2]);
+    ofname = argv[3];
+  } else {
+    cout << "no parameters provided" << endl;
+    exit(0);
+  }
+	double m0=1.989*pow(10,30);
+	vector <planet> p;
+	p.resize(8);
+	double vx0,vy0;
+	
+	p[0].m=3.285*pow(10,23); //Меркурий
+	p[0].r=57.91*pow(10,9);
+	p[0].phi=30*3.14159265/180;
+	p[0].per=88*86400;
+	p[0].omega=2*3.14159265/p[0].per;
+	
+	p[1].m=4.86*pow(10,24); //Венера
+	p[1].r=108000000000.0;
+	p[1].phi=-47*3.14159265/180;
+	p[1].per=225*86400;
+	p[1].omega=2*3.14159265/p[1].per;
+	
+	p[2].m=5.97*pow(10,24); //Земля
+	p[2].r=149600000000.0;
+	p[2].phi=0;
+	p[2].per=365*86400;
+	p[2].omega=2*3.14159265/p[2].per;
+	
+	p[3].m=6.39*pow(10,23); //Марс
+	p[3].r=228000000000.0;
+	p[3].phi=75*3.14159265/180;
+	p[3].per=687*86400;
+	p[3].omega=2*3.14159265/p[3].per;
+	
+	p[4].m=1.89*pow(10,27); //Юпитер
+	p[4].r=778570000000.0;
+	p[4].phi=95*3.14159265/180;
+	p[4].per=4332*86400;
+	p[4].omega=2*3.14159265/p[4].per;
+	
+	p[5].m=5.68*pow(10,26); //Сатурн
+	p[5].r=1430000000000.0;
+	p[5].phi=110*3.14159265/180;
+	p[5].per=10759*86400;
+	p[5].omega=2*3.14159265/p[5].per;
+	
+	p[6].m=8.68*pow(10,25); //Уран
+	p[6].r=2800000000000.0;
+	p[6].phi=-150*3.14159265/180;
+	p[6].per=30685*86400;
+	p[6].omega=2*3.14159265/p[6].per;
+	
+	p[7].m=1.02*pow(10,26); //Нептун
+	p[7].r=4550000000000.0;
+	p[7].phi=165*3.14159265/180;
+	p[7].per=60190*86400;
+	p[7].omega=2*3.14159265/p[7].per;
+	
+	for(int i=0;i<8;i++)
+	{
+		p[i].x=(p[i].r)*cos(p[i].phi);
+		p[i].y=(p[i].r)*sin(p[i].phi);
+		p[i].v=2*3.14159265*p[i].r/p[i].per;
+		p[i].vx=(p[i].v)*sin(p[i].phi);
+		p[i].vy=(p[i].v)*cos(p[i].phi);
+	}
+	
+	/*double x1,y1,vx1,vy1;
+	x=149600000000.0;
+	y=0;
+	vx=0;
+	vy=sqrt(6.67*pow(10,-11)*2*pow(10,30)/sqrt(x*x+y*y));*/
+	double x0,y0,fx,fy;
+	//ifstream in("input.txt");
+	//in>>vx0>>vy0;
+  vx0 = v0 * cos(angle);
+  vy0 = v0 * sin(angle);
+	fx=0;
+	fy=0;
+	x0=p[2].x;
+	y0=p[2].y;
+	vx0+=p[2].vx;
+	vy0+=p[2].vy;
+	ofstream out(ofname);
+	ofstream out1("mercury.txt");
+	ofstream out2("venus.txt");
+	ofstream out3("earth.txt");
+	ofstream out4("mars.txt");
+	ofstream out5("jup.txt");
+	ofstream out6("sat.txt");
+	ofstream out7("ur.txt");
+	ofstream out8("nept.txt");
+  ofstream spd("speed");
+	
+	for(long long i=0;i<=86400*365*6;i+=3600)
+	{
+		for(int j=0;j<8;j++)
+		{
+			p[j].x=(p[j].r)*cos((p[j].omega)*i+p[j].phi);
+			p[j].y=(p[j].r)*sin((p[j].omega)*i+p[j].phi);
+			p[j].vx=(p[j].v)*sin((p[j].omega)*i+p[j].phi);
+			p[j].vy=(p[j].v)*cos((p[j].omega)*i+p[j].phi);
+		}
+		x0+=vx0*3600;
+		y0+=vy0*3600;
+		vx0+=(-6.67*pow(10,-11)*m0*pow(x0*x0+y0*y0,-1.5)*x0*3600);
+		vy0+=(-6.67*pow(10,-11)*m0*pow(x0*x0+y0*y0,-1.5)*y0*3600);
+		for(int j=0;j<8;j++)
+		{
+			vx0+=(6.67*pow(10,-11)*p[j].m*pow((p[j].x-x0)*(p[j].x-x0)+(p[j].y-y0)*(p[j].y-y0),-1.5)*(p[j].x-x0)*3600);
+			vy0+=(6.67*pow(10,-11)*p[j].m*pow((p[j].x-x0)*(p[j].x-x0)+(p[j].y-y0)*(p[j].y-y0),-1.5)*(p[j].y-y0)*3600);
+		}
+			out<<i<<' '<<x0/1496/pow(10,8)<<' '<<y0/1496/pow(10,8)<<endl;		
+			out1<<i<<' '<<p[0].x/1496/pow(10,8)<<' '<<p[0].y/1496/pow(10,8)<<' '<<sqrt(p[0].x*p[0].x+p[0].y*p[0].y)/1496/pow(10,8)<<' '<<endl;
+			out2<<i<<' '<<p[1].x/1496/pow(10,8)<<' '<<p[1].y/1496/pow(10,8)<<' '<<sqrt(p[1].x*p[1].x+p[1].y*p[1].y)/1496/pow(10,8)<<' '<<endl;
+			out3<<i<<' '<<p[2].x/1496/pow(10,8)<<' '<<p[2].y/1496/pow(10,8)<<' '<<sqrt(p[2].x*p[2].x+p[2].y*p[2].y)/1496/pow(10,8)<<' '<<endl;
+			out4<<i<<' '<<p[3].x/1496/pow(10,8)<<' '<<p[3].y/1496/pow(10,8)<<' '<<sqrt(p[3].x*p[3].x+p[3].y*p[3].y)/1496/pow(10,8)<<' '<<endl;
+			out5<<i<<' '<<p[4].x/1496/pow(10,8)<<' '<<p[4].y/1496/pow(10,8)<<' '<<sqrt(p[4].x*p[4].x+p[4].y*p[4].y)/1496/pow(10,8)<<' '<<endl;
+			out6<<i<<' '<<p[5].x/1496/pow(10,8)<<' '<<p[5].y/1496/pow(10,8)<<' '<<sqrt(p[5].x*p[5].x+p[5].y*p[5].y)/1496/pow(10,8)<<' '<<endl;
+			out7<<i<<' '<<p[6].x/1496/pow(10,8)<<' '<<p[6].y/1496/pow(10,8)<<' '<<sqrt(p[6].x*p[6].x+p[6].y*p[6].y)/1496/pow(10,8)<<' '<<endl;
+			out8<<i<<' '<<p[7].x/1496/pow(10,8)<<' '<<p[7].y/1496/pow(10,8)<<' '<<sqrt(p[7].x*p[7].x+p[7].y*p[7].y)/1496/pow(10,8)<<' '<<endl;
+	}
+	double spd_fin;
+  spd_fin = sqrt(vx0 * vx0 + vy0 * vy0);
+  spd << spd_fin;
+	//in.close();
+	out1.close();
+	out2.close();
+	out3.close();
+	out4.close();
+	out5.close();
+	out6.close();
+	out7.close();
+	out8.close();
+	out.close();
+  spd.close();
+	return 0;
+}
